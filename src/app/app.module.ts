2848efc7048e4dcde9cca2becf5bce9823875a8b
Roland Jaggi <roland.jaggi@vzch.com>
2020-03-21 09:34:33 +0100

    added todo component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './component/app/app.component';
+import { TodoInputComponent } from './component/todo-input/todo-input.component';
+import { TodoListComponent } from './component/todo-list/todo-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoInputComponent,
+    TodoListComponent
   ],
   imports: [
     BrowserModule,
