48f2b775d9b4678df28f0811bba54a2c34796c8f
Roland Jaggi <roland.jaggi@vzch.com>
2020-03-22 18:31:39 +0100

    creating the components header.component.* and footer.component.* - creating navbar for the header - creating the footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule,  } from '@angular/core';
 
+import { HeaderComponent } from './component/navigation/header/header.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './component/app/app.component';
-import { TodoInputComponent } from './component/todo-input/todo-input.component';
-import { TodoListComponent } from './component/todo-list/todo-list.component';
+import { TodoInputComponent } from './component/favorite/todo-input/todo-input.component';
+import { TodoListComponent } from './component/favorite/todo-list/todo-list.component';
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { FooterComponent } from './component/navigation/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
     TodoInputComponent,
     TodoListComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
