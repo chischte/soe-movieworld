91f67fcde51c8a958da425546ff60fc5216b664f
Michael Wettstein <chischte@gmx.ch>
2020-05-13 08:02:33 +0200

    Copy content of "BEST" to "POPULÄR" as a starting point

--- src/app/component/menu/popular/popular.component.ts
+++ src/app/component/menu/popular/popular.component.ts
@@ -1,4 +1,8 @@
 import {Component, OnInit} from '@angular/core';
+import {ITopRated} from '../../../model/ITopRated';
+import {MovieDataServiceService} from '../../../service/movie-data-service.service';
+import {MovieFavoriteService} from '../../../service/movie-favorite.service';
+import {ITopRatedPage} from '../../../model/ITopRatedPage';
 
 @Component({
   selector: 'app-popular',
@@ -6,11 +10,56 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./popular.component.css']
 })
 export class PopularComponent implements OnInit {
+  topRated$: ITopRated[] = [];
+  genre: number;
+  topRatedImageBasePath: string = 'http://image.tmdb.org/t/p/w200/';
+  displayMode: number = 1;
 
-  constructor() {
+  constructor(private movieDataServiceService: MovieDataServiceService, private movieFavoriteService : MovieFavoriteService) {
   }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    return this.movieDataServiceService.getTopRatedPage()
+      .subscribe((data: ITopRatedPage) => {
+        this.topRated$ = data.results;
+      });
+  }
+  addFavorite(index) {
+    if (index >= 0) {
+      let resultOfIndex = this.topRated$[index];
+
+      const favorite = {movieName: resultOfIndex.title, additionalNotes: resultOfIndex.release_date};
+      console.log(favorite);
+      this.movieFavoriteService.insertFavorite(favorite)
+        .subscribe((response: any) => {})
+
+      alert('"' + resultOfIndex.title + '"' + " wurde in die Favoriten aufgenommen ");
+    } else {
+      alert("Der Film konnte nicht in die Favoriten gespeichert werden.");
+    }
+  }
+
+  onDisplayModeChange(mode: number): void {
+    this.displayMode = mode;
+  }
+
+  getGenrePipe(action: string) {
+    // TODO: use this.topRated$ variable for the genre pipe do not use the original one because you would overwrite it
+    let topGenreRated = this.topRated$;
+
+    // let allAvailableGenres = this.movieDataServiceService.getGenre()
+    //   .subscribe((data: IGenre) => {
+    //     this.genre = data.id;
+    //   });
+
+    // console.log(allAvailableGenres);
+
+    // topRated.forEach(function (value) {
+    //   console.log(value.genre_ids);
+    //   topRatedGenreIds += value.genre_ids
+    // });
+
+    console.log("GenreButtonClickWorked");
   }
 
 }
