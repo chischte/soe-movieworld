776a5c7b888ed3d9e7f933d8f6cee8ef41da9a44
Michael Wettstein <chischte@gmx.ch>
2020-05-13 09:02:07 +0200

    Add API String for page "popular"

--- src/app/service/movie-data-service.service.ts
+++ src/app/service/movie-data-service.service.ts
@@ -3,6 +3,7 @@ import {HttpClient} from "@angular/common/http";
 import {IGenre} from "../model/IGenre";
 import {ITopRatedPage} from "../model/ITopRatedPage";
 import {ServiceHelper} from "../serviceHelper/service-helper";
+import {IPopularPage} from '../model/IPopularPage';
 
 @Injectable({
   providedIn: 'root'
@@ -28,10 +29,12 @@ export class MovieDataServiceService {
   }
 
   getPopularPage(){
-    let firstPath:string  = "movie/top_rated?";
+   let firstPath:string  = "movie/top_rated?";
     let page:string = "&page=1";
-    let topRated:string = this.serviceHelper.createUrlPath(firstPath, "", this.language, page);
-    return this.httpClient.get<ITopRatedPage>(topRated);
+    //let sortby:string="";
+    //let popular:string = this.serviceHelper.createUrlPath(firstPath, sortby, this.language, page);
+    let popular:string="https://api.themoviedb.org/3/discover/movie?api_key=9daf07285a3560ae7a1a515899ab5db5&language=en-US&sort_by=popularity.desc&include_adult=true&include_video=false&page=1";
+    return this.httpClient.get<IPopularPage>(popular);
   }
 
 }
